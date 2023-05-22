167ad7c51a564dedeb470884eae903909cb15ad5
Misha <term11term@gmail.com>
2023-05-22 15:52:00 +0300

    account info микросервис и конфигурация для него

---
+++ account-info-microservice/src/main/java/com/lemi/accountinfomicroservice/AccountInfoMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.lemi.accountinfomicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountInfoMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountInfoMicroserviceApplication.class, args);
+    }
+
+}
---
+++ account-info-microservice/src/test/java/com/lemi/accountinfomicroservice/AccountInfoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemi.accountinfomicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountInfoMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
